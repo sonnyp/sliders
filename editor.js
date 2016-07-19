0bd292d795bebca8b633295919dee7588391ae82
Sonny Piers <sonny@fastmail.net>
2016-07-19 11:50:31 +0200

    better

--- editor.js
+++ editor.js
@@ -1,109 +1,99 @@
-'use strict';
+(function (window, document) {
+  'use strict';
 
-var myDoc = {
-  title: "My document",
-  author: "Sonny Piers",
-  frames: [
-    {
-      content: content1
-    },
-  ]
-}
+  const template = `
+    <h1>title</h1>
+    <img src="tux.png" style="width: 60px;"/>
+    <ol>
+      <li>ordered list</li>
+      <li>ordered list</li>
+    </ol>
+    <ul>
+      <li>unordered list</li>
+      <li>unordered list</li>
+    </ul>
+    <a href="http://wikipedia.org">a link</a>
+    <p><strong>hello</strong> dear <cite>world</cite></p>
+  `
 
-window.URL = window.URL || window.webkitURL
+  const frames = []
 
-function test() {
-  var elem = document.getElementById("editor");
-  elem.style.backgroundColor = 'white';
-  elem.contentEditable = false;
-  elem.requestFullScreen = elem.requestFullScreen || elem.mozRequestFullScreen || webkitRequestFullScreen;
-  elem.requestFullScreen();
-}
+  window.URL = window.URL || window.webkitURL
 
-var Editor = {
-  newFrame: function() {
-    var id = myDoc.frames.length;
-    var frame = {
-      content: defaultContent
-    };
-    myDoc.frames.push(frame);
-    UI.addThumbnail(id);
-    UI.switchToFrame(id);
-    editor2Canvas();
-  },
-}
+  const editor = document.getElementById('editor')
 
-var UI = {
-  addThumbnail: function(aFrameId) {
-    var li = document.createElement('li');
-    li.addEventListener('click', function() {
-      var frameId = this.getAttribute('data-frame-id');
-      UI.switchToFrame(frameId);
-      
-    });
-    li.classList.add('thumbnail');
-    li.setAttribute('data-frame-id', aFrameId);
-    var img = document.createElement('img');
-    img.src = "white.png"
-    li.appendChild(img);
-    var thumbnails = document.getElementById('thumbnails');
-    thumbnails.insertBefore(li, document.getElementById('newFrame'));
-  },
-  switchToFrame: function(aFrameId) {
-    UI.editor.innerHTML = myDoc.frames[aFrameId].content;
-    UI.editor.setAttribute('data-frame-id',  aFrameId);
-    var thumbnails = document.querySelectorAll('.thumbnail');
-    var thumbnail = document.querySelector('.thumbnail[data-frame-id="' + aFrameId + '"]');
-    for (var i = 0; i < thumbnails.length; i++) {
-      thumbnails[i].classList.remove('active');
+  // fullscreen preview
+  const prefix = ['moz', 'webkit'].find((prefix) => {
+    return editor[prefix + 'RequestFullScreen']
+  })
+  function preview () {
+    editor.contentEditable = false
+    editor[prefix + 'RequestFullScreen']()
+  }
+  document.addEventListener(prefix + 'fullscreenchange', () => {
+    if (
+      !document[`${prefix}FullScreenEnabled`] ||
+      document[`${prefix}FullScreenElement`] !== editor) {
+      editor.contentEditable = true;
     }
-    thumbnail.classList.add('active');
-  }
-}
-document.addEventListener('mozfullscreenchange', function() {
-  if (!document.mozFullScreenElement) {
-    UI.editor.contentEditable = true;
-  }
-});
-window.addEventListener('load', function() {
-  //~ Aloha.jQuery('#editor').aloha();
-  //~ return;
-  
-  UI.editor = document.getElementById('editor');
-  var editor = UI.editor;
-
-  var i = 0;
-  myDoc.frames.forEach(function(frame) {
-    UI.addThumbnail(i);
-    i++;
   });
+  document.getElementById('preview').addEventListener('click', preview)
+
+  function fullscreen () {
+    document.body[prefix + 'RequestFullScreen']()
+  }
+  document.getElementById('fullscreen').addEventListener('click', fullscreen)
+
+  const Editor = {
+    newFrame: function() {
+      const id = frames.length;
+      const frame = {
+        content: template
+      };
+      frames.push(frame);
+      UI.addThumbnail(id);
+      UI.switchToFrame(id);
+      // editor2Canvas();
+    },
+  }
+
+  const UI = {
+    editor,
+    addThumbnail: function(aFrameId) {
+      const li = document.createElement('li');
+      li.addEventListener('click', function() {
+        const frameId = this.getAttribute('data-frame-id');
+        UI.switchToFrame(frameId);
+      });
+      li.classList.add('thumbnail');
+      li.setAttribute('data-frame-id', aFrameId);
+      const img = document.createElement('img');
+      img.src = "white.png"
+      li.appendChild(img);
+      const thumbnails = document.getElementById('thumbnails');
+      thumbnails.insertBefore(li, document.getElementById('newFrame'));
+    },
+    switchToFrame: function(aFrameId) {
+      editor.innerHTML = frames[aFrameId].content;
+      editor.setAttribute('data-frame-id',  aFrameId);
+      const thumbnails = document.querySelectorAll('.thumbnail');
+      const thumbnail = document.querySelector('.thumbnail[data-frame-id="' + aFrameId + '"]');
+      for (let i = 0; i < thumbnails.length; i++) {
+        thumbnails[i].classList.remove('active');
+      }
+      thumbnail.classList.add('active');
+    }
+  }
 
   //Editor stuff
-  var observer = new window.MutationObserver(function(mutations) {
-    var foo = false;
-    if (mutations.length === 1) {
-      if (mutations[0].target === editor) {
-        return foo = true;
-      }
-      if (mutations[0].type === 'attributes') {
-        if (mutations[0].attributeName === '_moz_resizing') {
-          return foo = true;
-        }
-      }
-    }
-    //~ else {
-      //~ mutations.forEach(function(mutation) {
-        //~ console.log(mutation.target);
-        //~ console.log(mutation.type);
-        //~ console.log(mutation.attributeName);
-      //~ })
-    //~ }
-    if (foo)
-      return;
+  const observer = new window.MutationObserver(mutations => {
+    const muts = mutations.filter(mut => {
+      return mut.type !== 'attributes' || mut.attributeName !== '_moz_resizing'
+    })
 
     editor2Canvas();
-    var frameId = UI.editor.getAttribute('data-frame-id');
-    myDoc.frames[frameId].content = UI.editor.innerHTML;
+    const frameId = editor.getAttribute('data-frame-id');
+    frames[frameId].content = editor.innerHTML;
   });
   observer.observe(editor, {
     childList: true,
@@ -112,90 +102,83 @@ window.addEventListener('load', function() {
     subtree: true,
     attributeOldValue: true,
     characterDataOldValue: true,
-    //~ DOMString[] attributeFilter
   });
   
-  UI.switchToFrame(0);
-  editor2Canvas();
-  //~ UI.editor.innerHTML = myDoc.frames[0].content;
-  //~ UI.editor.setAttribute('data-frame-id', 0);
-  
+
   //newFrame
-  var newPageButton = document.getElementById('newFrame');
+  const newPageButton = document.getElementById('newFrame');
   newPageButton.addEventListener('click', function() {
     Editor.newFrame();
   });
 
-
-});
-
-var HTML2Canvas = function(aHTML) {
-  
-}
-
-function replaceImages (doc) {
-  var imgs = [...doc.querySelectorAll('img')]
-  var promises = imgs.map(function (img) {
-    return new Promise(function (resolve) {
-      img.removeAttribute('_moz_resizing');
-      var canvas = document.createElement('canvas')
-      canvas.width = img.width;
-      canvas.height = img.height;
-      var ctx = canvas.getContext('2d');   
-      ctx.drawImage(img, 0, 0, img.width, img.height);
-      canvas.toBlob(function (blob) {
-        img.src = window.URL.createObjectURL(blob)
-        img.addEventListener('load', function () {
-          window.URL.revokeObjectURL(blob)
-        })
+  function promiseLoaded (img) {
+    return new Promise((resolve, reject) => {
+      if (img.complete) {
         resolve()
-      }, 'image/png')
+      } else {
+        img.addEventListener('load', resolve)
+      }
     })
-  })
-  return Promise.all(promises)
-}
+  }
 
-function editor2Canvas () {
-  const editor = document.getElementById('editor');
-  const doc = editor.cloneNode(true);
-  doc.contentEditable = false;
+  /*
+   * Make imgs "screenshot-able"
+   * SVG foreignObject doesn't allow external resources even from the same origin
+   * https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Drawing_DOM_objects_into_a_canvas#Security
+   * so we replace their src properties with the blob url equivalent
+   */
+  function replaceImages (doc) {
+    const imgs = [...doc.querySelectorAll('img')]
+    return Promise.all(imgs.map(img => {
+      return promiseLoaded(img).then(() => {
+        return new Promise((resolve) => {
+          img.removeAttribute('_moz_resizing');
+          const canvas = document.createElement('canvas')
+          canvas.width = img.width;
+          canvas.height = img.height;
+          const ctx = canvas.getContext('2d');
+          ctx.drawImage(img, 0, 0, img.width, img.height);
+          console.log('image')
+          canvas.toBlob(blob => {
+            console.log('blob')
+            img.addEventListener('load', function () {
+              window.URL.revokeObjectURL(blob)
+            })
+            img.src = window.URL.createObjectURL(blob)
+            resolve()
+          }, 'image/png')
+        })
+      })
+    }))
+  }
 
-  const frameId = editor.getAttribute('data-frame-id');
+  function editor2Canvas () {
+    const editor = document.getElementById('editor');
+    const doc = editor.cloneNode(true);
+    doc.contentEditable = false;
 
-  replaceImages(doc).then(function () {
-    var canvas = document.createElement('canvas');
-    var ctx = canvas.getContext("2d");
-    var oSerializer = new XMLSerializer();
-    var sXML = oSerializer.serializeToString(doc); 
+    const frameId = editor.getAttribute('data-frame-id');
+    replaceImages(doc).then(function () {
+      const canvas = document.createElement('canvas');
+      const ctx = canvas.getContext("2d");
+      const sXML = (new XMLSerializer()).serializeToString(doc)
+      const data = `
+        <svg xmlns='http://www.w3.org/2000/svg' width='600' height='600'> 
+          <foreignObject width='100%' height='100%'>
+            ${sXML}
+          </foreignObject>,
+        </svg>
+      `
 
-    var data = [
-      "<svg xmlns='http://www.w3.org/2000/svg' width='600' height='600'>", 
-        "<foreignObject width='100%' height='100%'>",
-          sXML,
-        "</foreignObject>",
-      "</svg>"
-    ].join('')
-
-
-    var blob = new window.Blob([data], {type: "image/svg+xml"});
-
-    // var blob = BB.getBlob("image/svg+xml");
-    
-    //~ var dataURI = 'data:image/svg+xml;base64,' + btoa(data);
-
-    var url = window.URL.createObjectURL(blob);
-    
-    var img = document.querySelector('.thumbnail[data-frame-id="' + frameId + '"]').firstChild;
-    img.onload = function() {  
-      window.URL.revokeObjectURL(url);  
-    };  
-    img.src = url || dataURI;
-  })
-  .catch(function (err) {
-    console.error(err)
-  })
-  
-
-  //~ img.src = dataURI;
-  //~ editor.contentEditabe = true;
-}
+      const blob = new window.Blob([data], {type: "image/svg+xml"});
+      const url = window.URL.createObjectURL(blob);
+      
+      const img = document.querySelector('.thumbnail[data-frame-id="' + frameId + '"]').firstChild;
+      img.addEventListener('load', () => window.URL.revokeObjectURL(url))
+      img.src = url;
+    })
+    .catch(function (err) {
+      console.error(err)
+    })
+  }
+}(this, this.document))
